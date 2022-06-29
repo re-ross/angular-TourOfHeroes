47fecbf70d08979091c81a9328fc6c08d128d831
Ryan Ross <ryan.ross@meyerdistributing.com>
2022-06-29 10:59:34 -0500

    created mock hero data and displayed list. able to select specific hero w/ details shown. added styles to hero list

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
+import { HEROES } from 'src/app/data/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +9,14 @@ import { Hero } from './hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
